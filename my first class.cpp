40de427f11ed686aa7fbb2299bed53a3e1c90fc1
Abir Howladar <abirhowlader922@gmail.com>
2025-09-04 20:56:23 +0600

    Add files via upload

---
+++ my first class.cpp
@@ -0,0 +1,14 @@
+#include "my first class.h"
+#include<iostream>
+using namespace std;
+
+my first class::my first class()
+{
+    //ctor
+    cout << "Inside the constructor";
+}
+
+//my first class::~my first class()
+//{
+//    //dtor
+//}
