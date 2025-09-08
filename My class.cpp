300916184939e38ca8dab0fa701f46a99c05a174
Abir Howladar <abirhowlader922@gmail.com>
2025-09-08 09:20:40 +0600

    Add files via upload

---
+++ My class.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+class student{
+    public:
+int id;
+double gpa;
+
+
+
+};
+
+int main()
+{
+  student Alim;
+  Alim.id=101;
+  Alim.gpa=5.00;
+
+  cout << Alim.id << " "  <<Alim.gpa;
+
+}
