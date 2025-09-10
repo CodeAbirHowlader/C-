afeede4a14b556375b500fbc0747c84916246863
Abir Howladar <abirhowlader922@gmail.com>
2025-09-10 22:44:02 +0600

    Add files via upload

---
+++ mylastclass.cpp
@@ -0,0 +1,12 @@
+#include "mylastclass.h"
+#include<iostream>
+using namespace std;
+
+Mylastclass::Mylastclass()
+{
+    cout<< "It is by constructor"<<endl;
+}
+void Mylastclass::display()
+{
+    cout<< "It is by display function";
+}
