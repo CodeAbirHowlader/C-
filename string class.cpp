fd4771100585951f31e04508bd59f4dde8afec5c
Abir Howladar <abirhowlader922@gmail.com>
2025-09-12 08:53:51 +0600

    Add files via upload

---
+++ string class.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string str1 = "abir";
+    string str2 = "Howlader";
+    string str3;
+
+    str3 = str1;
+    cout << "str3 = "<<  str3 << endl;
+}
